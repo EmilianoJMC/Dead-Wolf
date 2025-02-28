253414f6c941172f539c7feae793136e19c8b327
EmilianoJMC <86630896+EmilianoJMC@users.noreply.github.com>
2025-02-27 23:08:58 -0300

    Ajustes

--- music_streaming/music_app/urls.py
+++ music_streaming/music_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AlbumViewSet, MusicViewSet
+from .views import AlbumViewSet, MusicViewSet, home, react_app
 
 router = DefaultRouter()
 router.register(r'albums', AlbumViewSet)
 router.register(r'musics', MusicViewSet)
 
 urlpatterns = [
+    path('home/', home, name='home'),  # Página inicial do Django
     path('api/', include(router.urls)),
+    path('', react_app, name='react_app'),  # React servido pelo Django
 ]
--- music_streaming/music_app/views.py
+++ music_streaming/music_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Album, Music
 from .serializers import AlbumSerializer, MusicSerializer
+from django.shortcuts import render
+from django.http import HttpResponse
 
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
@@ -9,3 +11,9 @@ class AlbumViewSet(viewsets.ModelViewSet):
 class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+def home(request):
+    return HttpResponse("Bem-vindo ao Music Streaming App!")
+
+def react_app(request):
+    return render(request, 'index.html')    
