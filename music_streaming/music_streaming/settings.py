2cc718f3ca20652d25d28d8d12361c94a99cc8b5
EmilianoJMC <86630896+EmilianoJMC@users.noreply.github.com>
2025-03-06 21:20:45 -0300

    teste

--- music_streaming/music_streaming/settings.py
+++ music_streaming/music_streaming/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music_app'
+    'music_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    
 ]
 
 ROOT_URLCONF = 'music_streaming.urls'
